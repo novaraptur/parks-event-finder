284f7a28cee996f20362d3d037118dc0f1c57e0b
Dean Cook <deancook371@gmail.com>
2021-08-05 07:41:00 -0700

    Add Router for main page and search page

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
+
+import Nav from '../Nav/Nav'
+import EventSearch from '../EventSearch/EventSearch';
 
 const App = ()=> {
   return (
-    <main>
-
-    </main>
+    <BrowserRouter>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={() => {
+            return (
+              <Nav />
+            )
+          }}
+        />
+        <Route
+          exact
+          path="/search"
+          render={() => {
+            return (
+              <EventSearch />
+            )
+          }}
+        />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
