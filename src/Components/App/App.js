6d05cc6d11da269ab4b540965378825873596b48
Dean Cook <deancook371@gmail.com>
2021-08-06 17:25:23 -0700

    Write test API call

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
 
@@ -12,6 +12,23 @@ import ParkDetailPage from '../ParkDetailPage/ParkDetailPage';
 import { getEvents, getSingleEvent, getSinglePark } from '../../api-calls/apiCalls';
 
 const App = () => {
+
+  const [savedParks, setSavedPark] = useState([]);
+  const [savedEvents, setSavedEvent] = useState([]);
+  const [allEvents, setAllEvents] = useState([]);
+
+  useEffect(() => {
+    getEvents('WA')
+      .then((data) => {
+        setAllEvents(data)
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  });
+
+  console.log(allEvents);
+
   return (
     <BrowserRouter>
       <Switch>
