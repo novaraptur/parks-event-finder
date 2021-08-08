bb89cbab92ac95823cf9e6f904d7558c7bc96972
Dean Cook <deancook371@gmail.com>
2021-08-08 14:27:58 -0700

    Flesh out detail page

--- src/Components/EventDetails/EventDetails.js
+++ src/Components/EventDetails/EventDetails.js
@@ -1,14 +1,32 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import { cleanEventData } from '../../utils/utils';
 
 const EventDetails = ({eventInfo}) => {
   const cleanEvent = cleanEventData(eventInfo.data[0]);
   console.log(cleanEvent);
+
+  const loadDates = () => {
+    return cleanEvent.dates.map((date) => {
+      return <li>{date}</li>;
+    })
+  }
+
+  const loadDescription = () => {
+    return <div dangerouslySetInnerHTML={{ __html: cleanEvent.description }} />
+  }
+
   return (
     <>
+      <NavLink to='/search'><button>Back</button></NavLink>
+      <img src={`https://www.nps.gov${cleanEvent.images[0].url}`} />
+      <button>Save Event</button>
       <h2>{cleanEvent.title}</h2>
       <h3>{cleanEvent.parkName}</h3>
+      {loadDates()}
+      <p>{cleanEvent.times[0].timestart} - {cleanEvent.times[0].timeend}</p>
+      {loadDescription()}
     </>
   );
 }
