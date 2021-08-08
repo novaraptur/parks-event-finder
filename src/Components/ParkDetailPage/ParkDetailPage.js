d1c5926de97c42e710dfcf93c456f4f184f7d2ca
Dean Cook <deancook371@gmail.com>
2021-08-08 16:39:44 -0700

    Add separate park details component, make API call to fetch single park data

--- src/Components/ParkDetailPage/ParkDetailPage.js
+++ src/Components/ParkDetailPage/ParkDetailPage.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import ParkDetails from '../ParkDetails/ParkDetails';
 import './ParkDetailPage.css';
 
-const ParkDetailPage = (code) => {
+import { getSingleParkByParkCode } from '../../api-calls/apiCalls';
+
+const ParkDetailPage = (parkCode) => {
+  const [parkInfo, setPark] = useState({});
+
+  useEffect(() => {
+    getSingleParkByParkCode(parkCode.code)
+      .then((data) => {
+        console.log(data);
+        setPark(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }, []);
+
   return (
     <h2>PARK DETAIL PAGE!</h2>
   );
