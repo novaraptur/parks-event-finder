557e0b28a1d70ccfd21ab0e81a67095f9e47f93d
Dean Cook <deancook371@gmail.com>
2021-08-09 09:06:47 -0700

    Style park cards

--- src/Components/ParkCard/ParkCard.js
+++ src/Components/ParkCard/ParkCard.js
@@ -3,12 +3,11 @@ import { NavLink } from 'react-router-dom';
 import './ParkCard.css';
 
 const ParkCard = ({park})=> {
-  console.log(park);
   return (
     <NavLink to={`/park/${park.parkCode}`}>
       <article id={park.id} className="park-card">
         <img src={`${park.images[0].url}`} className="park-card-image" />
-        <div className="event-card-text">
+        <div className="park-card-text">
           <h3>{park.fullName}</h3>
           <p>{park.states}</p>
         </div>
