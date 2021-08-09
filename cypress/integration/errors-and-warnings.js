7c9ade7d79b4f54ef0988eb0f9662fa31b7aa755
Dean Cook <deancook371@gmail.com>
2021-08-09 13:12:44 -0700

    Describe intial tests for errors and warnings

--- cypress/integration/errors-and-warnings.js
+++ cypress/integration/errors-and-warnings.js
@@ -4,4 +4,24 @@ describe('Errors and warnings', () => {
     cy.get('button').contains('See Events').click();
     cy.url().should('eq', 'http://localhost:3000/');
   });
+
+  it('should display a message to the user if they have no saved events', () => {
+    cy.visit('http://localhost:3000/saved-events');
+  });
+
+  it('should display a message to the user if they have no saved parks', () => {
+    cy.visit('http://localhost:3000/saved-parks');
+  });
+
+  it('should display a warning if there is no data for a park', () => {
+    cy.visit('http://localhost:3000/parks/kdau');
+  });
+
+  it('should display a warning if there is no data for an event', () => {
+    cy.visit('http://localhost:3000/events/aoobgawepauwgpaohwedknaf');
+  });
+
+  it('should display an error message if the data cannot be loaded from the API', () => {
+    
+  });
 });
