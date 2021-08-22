e2f0bde40e51b0115554539ace510178b94e76a0
Dean Cook <deancook371@gmail.com>
2021-08-22 10:33:53 -0700

    Remove out of date tests

--- cypress/integration/detail-pages.js
+++ cypress/integration/detail-pages.js
@@ -11,19 +11,6 @@ describe('Event detail page', () => {
     cy.get('button').contains('Back').click();
     cy.url().should('eq', 'http://localhost:3000/search');
   });
-
-  it('should allow the user to click a button to save the event', () => {
-    cy.get('button').contains('Save Event').click();
-    cy.get('button').contains('Back').click();
-    cy.get('li').contains('Saved Events').click();
-    cy.get('h3').contains('Evening Program');
-    cy.get('p').contains('Curecanti National Recreation Area');
-  });
-
-  it('should allow the user to click a link to view details about the park', () => {
-    cy.get('h3').contains('Curecanti National Recreation Area').click();
-    cy.url().should('eq', 'http://localhost:3000/park/cure');
-  });
 });
 
 describe('Park detail page', () => {
